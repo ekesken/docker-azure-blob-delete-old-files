90c852f930b624fd5033b14079b6d2af84c0ade2
Erhan Kesken <ekesken@users.noreply.github.com>
2016-08-25 13:35:38 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ if [ "${DRY_RUN}" = "true" ]; then
   DELETE_CMD="echo ${DELETE_CMD}";
 fi
 azure storage blob list ${AZURE_STORAGE_CONTAINER} \
- | cut -d ' ' -f5,13-18 | grep GMT | sed 's/ /\t/' \
+ | grep GMT | cut -d ' ' -f5,13-18 | sed 's/ /\t/' \
  | python filter_old_files.py | awk '{print $1}' \
  | while read FILE; do ${DELETE_CMD} ${FILE}; done
